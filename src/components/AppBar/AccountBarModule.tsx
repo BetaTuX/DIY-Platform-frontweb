0600c322c73233952a70d7f2994ff70ae413f493
Argann Bay-Eymère <argann.bay-eymere@epitech.eu>
2021-01-27 11:48:43 +0000

    + Added Login and Register button in the navbar

--- src/components/AppBar/AccountBarModule.tsx
+++ src/components/AppBar/AccountBarModule.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import {getBoolean} from "../../utils/boolean";
-import {Avatar, Dropdown, Menu, Skeleton} from "antd";
+import {Avatar, Button, Dropdown, Menu, Skeleton} from "antd";
 import {DownOutlined, SmileTwoTone} from "@ant-design/icons/lib";
+import {Link} from "react-router-dom";
 
 type Props = {};
 type State = {
@@ -28,8 +29,7 @@ export class AccountBarModule extends React.Component<Props, State> {
         };
     }
 
-
-    render() {
+    private loggedInModule() {
         return (
             <Skeleton loading={this.state.isLoading} paragraph={false} title={{width: 120}} avatar={{shape: "circle"}}>
                 <Dropdown placement={"bottomRight"} className={'dropdown'} overlay={menu}>
@@ -40,6 +40,19 @@ export class AccountBarModule extends React.Component<Props, State> {
                     </div>
                 </Dropdown>
             </Skeleton>
-        );
+        )
+    }
+
+    private linkModule() {
+        return (
+            <>
+                <Link to={'/register'}><Button className={"register-btn"} type={"ghost"}>Register</Button></Link>
+                <Link to={'/login'}><Button className={"login-btn"} type={"text"}>Login</Button></Link>
+            </>
+        )
+    }
+
+    render() {
+        return this.state.isLoggedIn ? this.loggedInModule() : this.linkModule();
     };
 }
--- src/components/AppBar/Navigation.tsx
+++ src/components/AppBar/Navigation.tsx
@@ -9,7 +9,6 @@ class Navigation extends Component {
             <ul className="horizontal">
                 <Link to={"/"}><Button type="text"><li>Home</li></Button></Link>
                 <Link to={"/test"}><Button type="text"><li>404 test</li></Button></Link>
-                <Link to={"/login"}><Button type="text"><li>Login/Register</li></Button></Link>
             </ul>
         );
     }
